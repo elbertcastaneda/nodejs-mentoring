63347f7a6663abbd87ecbdb26ca1365e7d55eb98
Elbert Castaneda <elbert_castaneda@gmail.com>
2020-09-19 02:09:33 -0500

    Homework 04.01 - Create 'groups' rest api

--- src/hw03/api/users/index.ts
+++ src/hw03/api/users/index.ts
@@ -2,6 +2,6 @@ import UserController from './user.controller';
 import createUserRepository from './user.repository';
 
 // Simple Dependency injection simulation
-const createUserModule = () => UserController.create(createUserRepository()).getRouter();
+const createUsersModule = () => UserController.create(createUserRepository()).getRouter();
 
-export default createUserModule;
+export default createUsersModule;
